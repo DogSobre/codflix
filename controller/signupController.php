78643cff6f4bd54422fcf621465ddea8305e8b1c
DogSobre <juanfernandoherrada13@gmail.com>
2020-06-23 12:47:56 +0200

    signup function, add new user into the database"

--- controller/signupController.php
+++ controller/signupController.php
@@ -2,23 +2,43 @@
 
 require_once( 'model/user.php' );
 
-/****************************
+/*****************************
 * ----- LOAD SIGNUP PAGE -----
-****************************/
+*****************************/
 
-function signupPage() {
 
-  $user     = new stdClass();
-  $user->id = isset( $_SESSION['user_id'] ) ? $_SESSION['user_id'] : false;
 
-  if( !$user->id ):
-    require('view/auth/signupView.php');
-  else:
-    require('view/homeView.php');
-  endif;
+
+function signupPage()
+{
+
+
+    $user = new stdClass();
+    $user->id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : false;
+
+    if (!$user->id):
+        require('view/auth/signupView.php');
+    else:
+        require('view/homeView.php');
+    endif;
+
+
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+
+
+    $userSignup = new user();
+    $userSignup->setEmail($email);
+    $userSignup->setPassword($password);
+
+    $userSignup->createUser();
+
+
 
 }
 
-/***************************
+/****************************
 * ----- SIGNUP FUNCTION -----
-***************************/
+****************************/
